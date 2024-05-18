0a7fe21753ba2def8a83df9f83a9d7d5a45abebb
email <khanhtoshiro2003@gmail.com>
2024-05-18 23:56:12 +0700

    Update index.js in Server

--- Server/index.js
+++ Server/index.js
@@ -10,12 +10,38 @@ import AuthRoutes from "./routes/auth.js"
 import RelationshipRoutes from "./routes/relationships.js";
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import {db} from "./connect.js"
+import multer from "multer";
+import { db } from "./connect.js"
 
 //middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.use(Express.json());
-app.use(cors());//giao tiếp tài nguyên
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);//giao tiếp tài nguyên
 app.use(cookieParser());//đọc các cookie từ người dùng
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../Client/public/upload");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/server/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
+
 
 app.use("/server/users", UsersRoutes);
 app.use("/server/posts", PostsRoutes);
@@ -25,10 +51,10 @@ app.use("/server/relationships", RelationshipRoutes);
 app.use("/server/reports", ReportsRoutes);
 app.use("/server/auth", AuthRoutes);
 
-app.listen(8800, ()=>{
+app.listen(8800, () => {
     console.log("Server is working!!!");
-    db.connect(function(err){
-        if(err) console.log(err);
+    db.connect(function (err) {
+        if (err) console.log(err);
         else console.log("Database is ok!!!");
     })
 });
