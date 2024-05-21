2f556a87fe1aaed144d030fe9252a2fc2b974875
email <khanhtoshiro2003@gmail.com>
2024-05-21 11:59:34 +0700

    Redirect to login if no user is logged in

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Home.css";
 import Leftbar from "../../components/leftbar/Leftbar.jsx";
@@ -7,35 +7,28 @@ import CreatePost from "../../components/createPost/CreatePost.jsx";
 import Posts from "../../components/posts/Posts.jsx";
 
 const Home = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("");
   const [posts, setPosts] = useState([]);
-  const navigate = useNavigate();
-
   const addNewPost = (newPost) => {
     setPosts([newPost, ...posts]);
   };
-
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("chat-user"))
+  );
   useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem("chat-user"));
     if (!currentUser) {
-      navigate("/login"); // Redirect to login if no user is logged in
+      navigate("/login");
     } else {
-      setCurrentUser(currentUser);
       const getUser = async () => {
-        try {
-          const res = await axios.get(
-            `http://localhost:8800/server/users/${currentUser.id}`,
-            { withCredentials: true }
-          );
-          setUser(res.data);
-        } catch (error) {
-          console.error("Error fetching user data:", error);
-        }
+        const res = await axios.get(
+          `http://localhost:8800/server/users/${currentUser.id}`,
+          { withCredentials: true }
+        );
+        setUser(res.data);
       };
       getUser();
     }
-  }, [navigate]);
-
+  }, []);
   return (
     <div className="home">
       {/* <Navbar user={user} /> */}
