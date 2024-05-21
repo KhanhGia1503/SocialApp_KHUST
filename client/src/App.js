fbb51ed62db29e841256f937cb8fc7f7aebe6760
duongkhanhb1k39 <duongkhanhb1k39@gmail.com>
2024-05-21 10:46:38 +0700

    add profile page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/home/Home";
 import ResetPassword from "./pages/resetpassword/ResetPassword";
 import ChangePassword from "./pages/resetpassword/ChangePassword";
 import Inbox from "./pages/chat/Inbox";
+import Profile from "./pages/profile/Profile";
 function App() {
   const router = createBrowserRouter([
     {
@@ -26,6 +27,7 @@ function App() {
     },
     { path: "/reset/:token", element: <ChangePassword></ChangePassword> },
     { path: "/inbox", element: <Inbox></Inbox> },
+    { path: "/:username", element: <Profile></Profile> },
   ]);
 
   return (
